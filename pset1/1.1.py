fecd3ba67ec8a9e25e00c2c40ee0c250d734ca79
Michael Yang <themichaelyang@gmail.com>
2018-10-29 01:06:25 -0700

    added newton's method equations

--- pset1/1.1.py
+++ pset1/1.1.py
@@ -19,3 +19,17 @@ print(X[:5])
 print(y[:5])
 print(theta)
 
+
+# ## Newton's method
+# 
+# Update rule: $\theta := \theta - H^{-1} \nabla_{\theta} l(\theta)$
+# 
+# Partial derivative vector and Hessian:
+# $$
+# \begin{aligned}
+# \nabla_{\theta} \ell (\theta)_{j} &= \sum_{i = 1}^m x_j^{(i)} (y^{(i)} - h_{\theta}(x^{(i)}))x_j^{(i)} \\
+# \\
+# H_{kj} &= \frac{\partial^2 \ell(\theta)}{\partial \theta_k \partial \theta_j} \\
+# &= \sum_{i = 1}^m x_j^{(i)} x_k^{(i)} g(\theta^T x^{(i)}) (1 - g(\theta^T x^{(i)})
+# \end{aligned}
+# $$
