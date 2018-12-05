8faadec18ea5d94b373446d0bee89776a65520b7
Michael Yang <themichaelyang@gmail.com>
2018-12-04 22:28:24 -0800

    added initial model code for functional regression

--- pset1/1.5c.py
+++ pset1/1.5c.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from collections import deque
+
+import pickle
+import os
 
 def get_weights(x_eval, X, tau):
     """
@@ -25,10 +29,19 @@ def pinv(mat):
     return np.linalg.pinv(mat) if mat.ndim > 1 else (1 / mat)
 
 
-def lw_regression(x_eval, X, Y, tau):
-    W = get_weights(x_eval, X, tau)
+def regression(X, Y, W=None):
+    if W is None:
+        m = X.shape[0]
+        W = np.eye(m)
     theta_pred = pinv(X.T @ W @ X) @ X.T @ W @ Y
     return theta_pred
+
+
+def lw_regression(x_eval, X, Y, tau):
+    W = get_weights(x_eval, X, tau)
+    return regression(X, Y, W)
+    # theta_pred = pinv(X.T @ W @ X) @ X.T @ W @ Y
+    # return theta_pred
 
 
 def prepend_ones(vec):
@@ -58,6 +71,48 @@ def split_data(data):
     return wavelengths, intensities
 
 
+def sq_distance(f1_y, f2_y):
+    dist = (f1_y - f2_y)
+    return dist @ dist
+
+
+def ker(t):
+    return max(1 - t, 0)
+
+
+class Model:
+    def __init__(Y_train, wavelengths):
+        """ Y_train is passed in as smoothed data here """
+        LYMAN_ALPHA = 1300
+        LYMAN_FOREST_MAX = 1200
+
+        self.wavelengths = wavelengths
+        self.right_start = np.where(wavelengths == LYMAN_ALPHA)[0][0]
+        self.left_end = np.where(wavelengths == LYMAN_FOREST_MAX)[0][0]
+
+        # Split data for f_left and f_right
+        # These Y values are the outputs of f (Y_train: absorption free data)
+        self.Y_train_right = Y_train[:, self.right_start:]
+        self.Y_train_left = Y_train[:, :self.left_end]
+
+
+    def predict(Y_right_pred):
+        """ Predict Y_left_pred from Y_right_pred (which has absorption) """
+        wavelengths_left = self.wavelengths[:self.left_end]
+        Y_left_pred = deque()
+
+        dists = sq_distance()
+
+        for wv in wavelengths_left:
+            y_i_pred = self.predict_pt(wv, Y_right_pred)
+            Y_left_pred.append()
+
+        return wavelengths_left, Y_left_pred
+
+    def predict_pt(x_pred, Y_right_pred):
+        pass
+
+
 def main():
     TAU = 5
 
@@ -72,9 +127,15 @@ def main():
     Y_train = intensities
     X_train = prepend_ones(wavelengths)
 
-    print(smooth(X_train, Y_train, TAU))
+    # Get smoothed Y of training set
+    Y_train_sm_file = 'Y_train_sm.pkl'
+    Y_train_sm = None
 
-
+    if not os.path.exists(Y_train_sm_file):
+        Y_train_sm = smooth(X_train, Y_train, TAU)
+        pickle.dump(Y_train_sm, open(Y_train_sm_file, 'wb'))
+    else:
+        Y_train_sm = pickle.load(open(Y_train_sm_file, 'rb'))
 
 
 if __name__ == "__main__":
